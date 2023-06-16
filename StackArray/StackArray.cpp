bc3ab1d5b2235271595cfe81e0fa93d1ebc8d21d
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-06-16 16:51:24 +0700

    membuat metode empty

--- StackArray/StackArray.cpp
+++ StackArray/StackArray.cpp
@@ -24,7 +24,16 @@ public:
 		cout << endl;
 		cout << element << "Ditambahkan(pushed)" << endl;		
 		}
+
 	void pop() {
+		if (empty()) {	//step 1
+			cout << "\nStack is empty. Cannot pop." << endl;				//1.a
+			return;		//1.b
+		}
+
+		cout << "\nThe popped element is : " << stack_array[top] << endl;	//step 2
+		top--;			//step 3 decrement
+	}
 
 	}
 }
