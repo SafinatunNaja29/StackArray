8361b084cea4cdf3dab2d2d2ee4118499188dde7
SafinatunNaja29 <safinatun.nj29@gmail.com>
2023-06-16 16:49:28 +0700

    menambahkan metode step 3

--- StackArray/StackArray.cpp
+++ StackArray/StackArray.cpp
@@ -19,6 +19,12 @@ public:
 			cout << "Number of data exceds the limit." << endl;
 			return;
 		}
+		top++;
+		stack_array[top] = element;	 	//step 3
+		cout << endl;
+		cout << element << "Ditambahkan(pushed)" << endl;
+		
+	}
 
 
 	}
